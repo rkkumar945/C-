7f54ba0aef30423c26b0a04e5c52ce10d3818db8
rkkumar945 <rkkumar8476@gmail.com>
2024-11-01 18:53:48 +0530

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a,b;
+	cout<<"enter two numbers";
+	cin>>a>>b;
+	cout<<"sum="<<a+b<<endl;
+	cout<<"diff="<<a-b<<endl;
+	cout<<"product="<<a*b<<"\n";
+	cout<<"division="<<a/b<<"\n";
+	cout<<"remainder="<<a%b<<"\n";
+	return 0;
+	
+}
